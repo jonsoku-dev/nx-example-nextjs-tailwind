380d13c569b3b32169588695d354975963bb013c
jonsoku2 <>
2022-07-10 01:56:14 +0900

    Understanding Next.js GetStaticProps
    https://egghead.io/lessons/next-js-understanding-next-js-getstaticprops

--- apps/site/pages/about/index.tsx
+++ apps/site/pages/about/index.tsx
@@ -1,14 +1,29 @@
+import { GetStaticProps } from 'next/types';
 import styles from './index.module.css';
 
 /* eslint-disable-next-line */
-export interface AboutProps {}
+export interface AboutProps {
+  name: string;
+}
 
-export function About(props: AboutProps) {
+export function About({ name }: AboutProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to About!</h1>
+      <h1>Welcome to {name}</h1>
     </div>
   );
 }
+
+/**
+ * - getStaticProps
+ * - getStaticPath
+ */
+export const getStaticProps: GetStaticProps<AboutProps> = async (context) => {
+  return {
+    props: {
+      name: 'Jongseok',
+    },
+  };
+};
 
 export default About;
